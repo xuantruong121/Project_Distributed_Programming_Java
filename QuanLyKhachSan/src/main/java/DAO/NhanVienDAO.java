0dae234a6476eaa1168fa8bfb3b550efd9e17e86
xuan truong <22714311.truong@student.iuh.edu.vn>
2025-01-23 19:33:55 +0700

    creating-new-branch-and-updating-code

--- QuanLyKhachSan/src/main/java/DAO/NhanVienDAO.java
+++ QuanLyKhachSan/src/main/java/DAO/NhanVienDAO.java
@@ -2,10 +2,6 @@ package DAO;
 
 import jakarta.persistence.EntityManager;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 3876203ba275795eb5c0448a2b130c2628fcbd7e
 public class NhanVienDAO {
     private EntityManager em =null;
     public NhanVienDAO() {
